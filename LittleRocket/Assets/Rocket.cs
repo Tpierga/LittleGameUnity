880700d0ca5350ef395a7cb0c9b337976aa05044
Tpierga <thibaultpierga@outlook.fr>
2020-04-18 18:56:05 +0200

    adding thruster sound

---
+++ LittleRocket/Assets/Rocket.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _audioSource.Play();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _audioSource.Stop();
+        }
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
